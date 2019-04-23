68cb72927bcd94f1659c6165d4ad2047d692ae95
hectormanuelmx <hectormanuelmx@outlook.com>
2019-04-23 00:00:33 -0500

    Imagen de fondo
    Se agrego la clase .hero-image para dar estilo a la imagen principal

---
+++ js/busqueda.js
@@ -0,0 +1,17 @@
+'use strict'
+//Metodos de ebusqueda en cadenas
+
+var texto = "Estaba mirando tus ojos y me fui, sintiendo tu sonrisa oyendote hablar...";
+
+var busqueda  = texto.match("hermosa");
+
+console.log(busqueda);
+
+
+//Metodo par areemplazar texto en ena cadena en JAVASCRIPT
+//Metodo .replace
+
+var texto2 = "La más hermosa";
+var busqueda = texto.replace("hermosa","chisquiada");
+console.log(busqueda);
+
